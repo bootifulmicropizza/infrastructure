d4a437d5d3ab7f2fefc23659eb9d87968c333fbd
Ian Collington <iancollington@gmail.com>
2019-01-23 22:43:48 +0000

    Remove ingress and simple web app

--- k8s-cluster/delete-cluster.sh
+++ k8s-cluster/delete-cluster.sh
@@ -14,12 +14,8 @@ AWS_REGION=eu-west-1
 function deleteCluster() {
 	echo "Deleting cluster $CLUSTER_NAME"
 
-	# Delete the ELB (to free up the network interfaces so the VPC can be deleted)
-	INGRESS_ELB=`kubectl get service ingress-nginx --namespace ingress-nginx -o jsonpath='{.status.loadBalancer.ingress[0].hostname}'`
-	echo "Got INGRESS_ELB $INGRESS_ELB"
-	LOAD_BALANCER_NAME=`aws elb describe-load-balancers --query "LoadBalancerDescriptions[?CanonicalHostedZoneName=='$INGRESS_ELB'].LoadBalancerName" --output text`
-	echo "Deleting ELB $LOAD_BALANCER_NAME"
-	aws elb delete-load-balancer --load-balancer-name $LOAD_BALANCER_NAME
+  kubectl delete -f nginx/
+  kubectl delete -f ingress/
 
 	# Delete stack from CF
 	aws cloudformation delete-stack --stack-name $STACK_NAME --region $AWS_REGION
